f7714858b91458db3c19adcd3b30167c4914b040
edoatley <edoatley@gmail.com>
2025-09-20 19:27:17 +0100

    remove profile

--- github/scripts/recover-failed-stack.sh
+++ github/scripts/recover-failed-stack.sh
@@ -5,7 +5,7 @@ set -eou
 
 echo "🚀 Checking stack status and cleaning up if necessary..."
 ########################################################################################################################
-STACK_STATUS=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query "Stacks[0].StackStatus" --output text --profile "$PROFILE" --region "$REGION" 2>/dev/null || echo "DOES_NOT_EXIST")
+STACK_STATUS=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query "Stacks[0].StackStatus" --output text --region "$REGION" 2>/dev/null || echo "DOES_NOT_EXIST")
 echo "  - Stack status: $STACK_STATUS"
 
 
@@ -16,9 +16,9 @@ if [ "$STACK_STATUS" == "ROLLBACK_COMPLETE" ] || [ "$STACK_STATUS" == "DELETE_FA
     bash ./github/scripts/empty-bucket.sh
 
     echo "🗑️ Deleting stack..."
-    aws cloudformation delete-stack --stack-name "$STACK_NAME" --profile "$PROFILE" --region "$REGION"
+    aws cloudformation delete-stack --stack-name "$STACK_NAME" --region "$REGION"
     echo "⏳ Waiting for stack to be deleted..."
-    aws cloudformation wait stack-delete-complete --stack-name "$STACK_NAME" --profile "$PROFILE" --region "$REGION"
+    aws cloudformation wait stack-delete-complete --stack-name "$STACK_NAME" --region "$REGION"
     echo "✅ Stack deleted."
 elif [ "$STACK_STATUS" != "DOES_NOT_EXIST" ] && [ "$STACK_STATUS" != "CREATE_COMPLETE" ] && [ "$STACK_STATUS" != "UPDATE_COMPLETE" ] && [ "$STACK_STATUS" != "UPDATE_ROLLBACK_COMPLETE" ]; then
     echo "⚠️ Stack is in an unrecoverable state ($STACK_STATUS). Please check the AWS console. Aborting."
